54544bb5a9ec2533f5c79c45b77629d6de2a0446
Luca D'Ettorre <ldettorre21@gmail.com>
2020-03-13 22:41:19 +0000

    Used the csv import to declare and write the headers. I then used a for loop to iterate over each skill and write the data to a new row

--- sitescraping.py
+++ sitescraping.py
@@ -7,6 +7,19 @@ response = requests.get("https://ldettorre.github.io/portfolio/")
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-first_scrape = soup.body.contents
+# Each skill section in the site is seperated via col-sm-4 so that's what we'll use to select them by.
+skills = soup.select(".col-sm-4")
 
-print(first_scrape)
+# Below creates a file with our chosen name and declares the headers we want to use.
+with open("data.csv", "w") as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ["Title", "Description", "Skill Name"]
+    csv_writer.writerow(headers)
+
+# It then iterates over each skill and writes the data to a row.
+    for skill in skills:
+        title = skill.find(class_="skill-title").get_text() 
+        desc = skill.find(class_="skill-description").get_text()
+        skill_name = skill.select(".skill-name")[0].get_text()
+        csv_writer.writerow([title, desc, skill_name])
+    
