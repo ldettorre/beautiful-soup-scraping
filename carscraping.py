790c9a4f0c18e198d97f7a1dcee43dc42c291fce
Luca D'Ettorre <ldettorre21@gmail.com>
2020-03-14 23:07:37 +0000

    Created a function to take a url and limit, it will then iterate upto the limit and add that iterations number to the end of the url. This means we can pass a predetermined url with the year and county included and then print the urls for many cars based on the limit.

--- carscraping.py
+++ carscraping.py
@@ -7,7 +7,14 @@ response = requests.get("https://www.motorcheck.ie/free-car-check/?vrm=07D54140"
 soup = BeautifulSoup(response.text, "html.parser")
 
 car_summary = soup.select(".report-summary")
+# print(car_summary)
 
-print(car_summary)
+# The function below takes a url address as a string and limit as an integer.
+# The loop will run upto the set limit -1 as it's a range. 
+# Each iteration will print the url with that iterations number concatenated to the end.
 
+def incrmenting_url(url, limit):
+    for i in range(limit):
+        print(url + str(i))
 
+incrmenting_url("https://www.motorcheck.ie/free-car-check/?vrm=07D",5)
