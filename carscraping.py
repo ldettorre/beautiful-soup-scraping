34686cf4109da82d405d635fdd3f7f9cbea29dca
Luca D'Ettorre <ldettorre21@gmail.com>
2020-03-15 02:26:48 +0000

    I can now write scraped data to a csv file and append new data onto it. No for loop was used in this instance due to the structure of the target data hence the current format on lines 14 - 19 of carscraping.py. Next step is to stop headers being copied with new data and begin integrating the url iteration somehow

--- carscraping.py
+++ carscraping.py
@@ -1,13 +1,24 @@
-import requests
+import requests, os
 from bs4 import BeautifulSoup
 from csv import writer
 
-response = requests.get("https://www.motorcheck.ie/free-car-check/?vrm=07D54140")
+response = requests.get("https://www.cartell.ie/ssl/servlet/beginStarLookup?registration=191D194")
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-car_summary = soup.select(".report-summary")
-# print(car_summary)
+
+with open("car_data.csv", "a") as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ["Registration","Make","Model","Description","Engine Capacity"]
+    csv_writer.writerow(headers)
+
+    registration = soup.select("td")[0].get_text()
+    make = soup.select("td")[1].get_text()
+    model = soup.select("td")[2].get_text()
+    desc = soup.select("td")[3].get_text()
+    engine_cc = soup.select("td")[4].get_text()
+    csv_writer.writerow([registration, make, model, desc, engine_cc])
+
 
 # The function below takes a url address as a string and limit as an integer.
 # The loop will run upto the set limit -1 as it's a range. 
@@ -17,4 +28,8 @@ def incrmenting_url(url, limit):
     for i in range(limit):
         print(url + str(i))
 
-incrmenting_url("https://www.motorcheck.ie/free-car-check/?vrm=07D",5)
+# incrmenting_url("https://www.cartell.ie/ssl/servlet/beginStarLookup?registration=191D",5)
+
+
+
+
